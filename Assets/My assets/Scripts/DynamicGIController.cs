79bcf681aeeb567297e03301a802bfd09c266e19
Yuma Yanagisawa <ya7gisa0@gmail.com>
2018-04-12 12:55:11 +0900

    Initial commit

---
+++ Assets/My assets/Scripts/DynamicGIController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicGIController : MonoBehaviour {
+    // change GI in run time
+    private Skybox skybox;
+    private Material skyboxMat;
+    private float t = 0.0f;
+    private Color currentSkyTint;
+    private Color initTint;
+
+    //D05353FF
+    Color colorA = new Color(208 / 256, 83 / 256, 189 / 256, 1.0f);
+    Color colorB = new Color(208 / 256, 83 / 256, 83 / 256, 1.0f);
+    
+
+    void Start()
+    {
+        // get a reference of the skybox
+        skybox = GetComponent<Skybox>();
+        skyboxMat = skybox.material;
+        string name = skybox.material.name;
+        Debug.Log(name);
+
+        // set inital sky tint
+        currentSkyTint = colorA;
+        initTint = currentSkyTint;
+        
+        //RenderSettings.skybox.SetColor("_Tint", new Color(0, 0, 0, 0));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        t += Mathf.Sin(Time.time);
+        // get color
+
+        currentSkyTint = Color.Lerp(colorA, colorB, t);
+        skyboxMat.SetColor("_SkyTint", currentSkyTint);
+	}
+}
---
+++ Assets/My assets/Scripts/jump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+    private float t = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // check the location of the game obj
+        t += Time.deltaTime;
+        if (t > 5.0f)
+        {
+            t = 0.0f;
+            // get pos
+            Vector3 pos = gameObject.transform.localPosition;
+            // move up
+            gameObject.transform.localPosition = new Vector3(pos.x, pos.y + Random.Range(5.0f, 12.0f), pos.z);
+            gameObject.transform.Rotate(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f));
+        }
+	}
+}
